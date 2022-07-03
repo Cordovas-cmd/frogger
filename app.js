efe3837bde33cc67311489b1eaff12167b678c83
samuel cordova <cordovas1337@gmail.com>
2022-07-03 18:07:28 -0500

    added more movement functionality

--- app.js
+++ app.js
@@ -5,23 +5,31 @@ const squares = document.querySelectorAll('.grid div');
 
 console.log(squares)
 let currentIndex = 76;
+const width = 9;
 
 // move frog using event 
 function moveFrog(e) {
-
     switch(e.key) {
+
         case 'ArrowLeft' :
-        console.log('Move left')
-        break
+            console.log('Move left')
+            currentIndex -= 1;
+            break;
+
         case 'ArrowRight' :
-        console.log('Move right')
-        break
+            console.log('Move right')
+            currentIndex += 1;
+            break;
+
         case 'ArrowUp' :
-        console.log('Move up')
-        break
+            console.log('Move up')
+            currentIndex -= width 
+            break;
+
         case 'ArrowDown' :
-        console.log('Move down')
-        break
+            console.log('Move down')
+            currentIndex += width
+            break;
     }
 squares[currentIndex].classList.add('frog');
 
