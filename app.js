b591e210f5b82b38ce2843d860ab44e302d910b1
samuel cordova <cordovas1337@gmail.com>
2022-07-03 18:01:09 -0500

    added more tests to make sure each arrow key logged the correct direction for movement

--- app.js
+++ app.js
@@ -3,9 +3,27 @@ const resultDiplay = document.querySelector('#result');
 const startPauseButton = document.querySelector('#start-pause-button');
 const squares = document.querySelectorAll('.grid div');
 
+console.log(squares)
+let currentIndex = 76;
 
-function moveFrog() {
- console.log('moved')
+// move frog using event 
+function moveFrog(e) {
+
+    switch(e.key) {
+        case 'ArrowLeft' :
+        console.log('Move left')
+        break
+        case 'ArrowRight' :
+        console.log('Move right')
+        break
+        case 'ArrowUp' :
+        console.log('Move up')
+        break
+        case 'ArrowDown' :
+        console.log('Move down')
+        break
+    }
+squares[currentIndex].classList.add('frog');
 
 }
 document.addEventListener('keyup', moveFrog)
